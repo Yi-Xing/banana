3a4f10bda4fdefd09cc12e9f62632899f130f075
yixing <yixing_zhang@foxmail.com>
2026-02-08 01:14:29 +0800

    feat 完善 dubbo 配置

--- banana-service/src/main/java/top/fblue/banana/BananaServiceApplication.java
+++ banana-service/src/main/java/top/fblue/banana/BananaServiceApplication.java
@@ -3,9 +3,11 @@ package top.fblue.banana;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDubbo
 @SpringBootApplication
+@ComponentScan(basePackages = {"top.fblue.framework"})
 public class BananaServiceApplication {
 
     public static void main(String[] args) {
